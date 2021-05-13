0c286633f471c15647d8f20f5b907c3dbcbb46ff
Joonas Kuusela <kuujoo@gmail.com>
2021-05-13 20:10:17 +0300

    Simple timer.

--- engine/include/pxl/time.h
+++ engine/include/pxl/time.h
@@ -9,5 +9,16 @@ namespace pxl
 		extern float delta;
 		extern float true_delta;
 		extern u64 ticks;
+
+		class Timer
+		{
+		public:
+			Timer();
+			void reset();
+			u64 ms();
+			u64 us();
+		private:
+			u64 start;
+		};
 	}
 }
--- engine/src/time.cpp
+++ engine/src/time.cpp
@@ -1,6 +1,30 @@
 #include <pxl/time.h>
+#include <chrono>
 
-const pxl::u64 pxl::time::ticks_per_second = 1000000;
-float pxl::time::delta = 0.0f;
-float pxl::time::true_delta = 0.0f;
-pxl::u64 pxl::time::ticks = 0;
+using namespace pxl;
+
+const u64 time::ticks_per_second = 1000000;
+float time::delta = 0.0f;
+float time::true_delta = 0.0f;
+u64 time::ticks = 0;
+
+time::Timer::Timer()
+{
+	reset();
+}
+
+void time::Timer::reset()
+{
+	start = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+u64 time::Timer::ms()
+{
+	auto u = us();
+	return u / 1000UL;
+}
+
+u64 time::Timer::us()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() - start;
+}
