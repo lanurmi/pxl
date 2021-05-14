5e0ac0dd9dcf2bd5fc2bc3fd487a1008064a0203
Joonas Kuusela <kuujoo@gmail.com>
2021-05-14 15:51:34 +0300

    Parser for ogmo-editor files.

---
+++ engine/include/pxl/image/ogmo.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <pxl/types.h>
+#include <pxl/math/color.h>
+
+namespace pxl
+{
+	struct Ogmo
+	{
+		struct Entity
+		{
+			string name;
+			int id;
+			string _eid;
+			int x;
+			int y;
+			int origin_x;
+			int origin_y;
+			map<string, string> string_text_enum_color_values;
+			map<string, bool> bool_values;
+			map<string, int> int_values;
+			map<string, float> float_values;
+
+		};
+		struct TileLayer
+		{
+			string name;
+			string _eid;
+			int offset_x;
+			int offset_y;
+			int grid_cell_width;
+			int grid_cell_height;
+			int grid_cells_x;
+			int grid_cells_y;
+			string tileset;
+			vector<int> data;
+			int export_mode;
+			int array_mode;
+		};
+		struct EntityLayer
+		{
+			string name;
+			string _eid;
+			int offset_x;
+			int offset_y;
+			int grid_cell_width;
+			int grid_cell_height;
+			int grid_cells_x;
+			int grid_cells_y;
+			std::vector<Entity> entities;
+		};
+
+		
+
+		Ogmo();
+		Ogmo(const string& path);
+		void load(const string& path);
+		bool valid() const { return !ogmo_version.empty(); }
+		string ogmo_version;
+		int width;
+		int height;
+		int offset_x;
+		int offset_y;
+		std::vector<TileLayer> tile_layers;
+		std::vector<EntityLayer> entity_layers;
+		map<string, string> string_text_enum_color_values;
+		map<string, bool> bool_values;
+		map<string, int> int_values;
+		map<string, float> float_values;
+	};
+}
---
+++ engine/src/image/ogmo.cpp
@@ -0,0 +1,116 @@
+#include <pxl/image/ogmo.h>
+#include <pxl/filesystem.h>
+#include <pxl/3rdparty/json.hpp>
+
+using namespace pxl;
+
+
+Ogmo::Ogmo()
+{
+
+}
+
+Ogmo::Ogmo(const string &path)
+{
+	load(path);
+}
+
+static void parseValues(std::map<string,string>& dstStrings,
+	std::map<string,int>& dstInt,
+	std::map<string,float>& dstFloats,
+	std::map<string,bool>& dstBools,
+	const nlohmann::json& valuesJson)
+{
+	for (auto itv = valuesJson.begin(); itv != valuesJson.end(); ++itv)
+	{
+		if (itv->is_string())
+		{
+			dstStrings[itv.key()] = itv.value();
+		}
+		else if (itv->is_number_float())
+		{
+			dstFloats[itv.key()] = itv.value();
+		}
+		else if (itv->is_number_integer())
+		{
+			dstInt[itv.key()] = itv.value();
+		}
+		else if (itv->is_boolean())
+		{
+			dstBools[itv.key()] = itv.value();
+		}
+	}
+}
+
+void Ogmo::load(const string& path)
+{
+	file::FileStream file(path, file::FileMode::Read);
+	string data = file.all();
+	nlohmann::json ogmoJson = nlohmann::json::parse(data);
+	
+	ogmo_version = ogmoJson["ogmoVersion"].get<string>();
+	width = ogmoJson["width"].get<int>();
+	height = ogmoJson["height"].get<int>();
+	offset_x = ogmoJson["offsetX"].get<int>();
+	offset_y = ogmoJson["offsetY"].get<int>();
+
+	if (ogmoJson.contains("values"))
+	{
+		parseValues(string_text_enum_color_values, int_values, float_values, bool_values, ogmoJson["values"]);
+	}
+	nlohmann::json ogmoLayers = ogmoJson["layers"];
+	assert(ogmoLayers.is_array());
+	for (auto &it : ogmoLayers)
+	{
+		if (it.contains("entities"))
+		{
+			EntityLayer layer;
+			layer.name = it["name"].get<string>();
+			layer._eid = it["_eid"].get<string>();
+			layer.offset_x = it["offsetX"].get<int>();
+			layer.offset_y = it["offsetY"].get<int>();
+			layer.grid_cell_width = it["gridCellWidth"].get<int>();
+			layer.grid_cell_height = it["gridCellHeight"].get<int>();
+			layer.grid_cells_x = it["gridCellsX"].get<int>();
+			layer.grid_cells_y = it["gridCellsY"].get<int>();
+			auto entities = it["entities"];
+			for (auto& e : entities)
+			{
+				Entity entity;
+				entity.name = e["name"].get<string>();
+				entity.id = e["id"].get<int>();
+				entity._eid = e["_eid"].get<string>();
+				entity.x = e["x"].get<int>();
+				entity.y = e["y"].get<int>();
+				entity.origin_x = e["originX"].get<int>();
+				entity.origin_y = e["originY"].get<int>();
+
+				if (e.contains("values"))
+				{
+					parseValues(entity.string_text_enum_color_values, entity.int_values, entity.float_values, entity.bool_values, e["values"]);
+				}
+			}
+		}
+		else if(it.contains("tileset"))
+		{
+			TileLayer layer;
+			layer.name = it["name"].get<string>();
+			layer._eid = it["_eid"].get<string>();
+			layer.offset_x = it["offsetX"].get<int>();
+			layer.offset_y = it["offsetY"].get<int>();
+			layer.grid_cell_width = it["gridCellWidth"].get<int>();
+			layer.grid_cell_height = it["gridCellHeight"].get<int>();
+			layer.grid_cells_x = it["gridCellsX"].get<int>();
+			layer.grid_cells_y = it["gridCellsY"].get<int>();
+			layer.tileset = it["tileset"].get<string>();
+			auto data = it["data"];
+			for (auto& d : data)
+			{
+				layer.data.push_back(d.get<int>());
+			}
+			layer.export_mode = it["exportMode"].get<int>();
+			layer.array_mode = it["arrayMode"].get<int>();
+			tile_layers.emplace_back(layer);
+		}
+	}
+}
