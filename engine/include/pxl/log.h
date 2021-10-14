823b2fd0d2c22a6c819fa0e5417381080b99ab9d
Joonas Kuusela <kuujoo@gmail.com>
2021-10-14 16:16:10 +0300

    Use logger interface

--- engine/include/pxl/log.h
+++ engine/include/pxl/log.h
@@ -6,6 +6,14 @@ namespace pxl
 {
 	namespace log
 	{
+		class ILogger {
+		public:
+			virtual void message(const String &message) = 0;
+			virtual void error(const String &message) = 0;
+		};
+		using ILoggerRef = std::shared_ptr<ILogger>;
+
+		void set(ILoggerRef logger);
 		void message(const String& message);
 		void error(const String& error);
 	};
--- engine/src/log.cpp
+++ engine/src/log.cpp
@@ -4,14 +4,36 @@
 
 using namespace pxl;
 
+namespace
+{
+	class CoutLogger : public pxl::log::ILogger {
+	public:
+		void message(const String &message)
+		{
+			std::cout << message << std::endl;
+		}
+		void error(const String &message)
+		{
+			std::cout << message << std::endl;
+		}
+	};
+
+	pxl::log::ILoggerRef logger = std::make_shared<CoutLogger>();
+}
+
+void log::set(pxl::log::ILoggerRef loggerRef)
+{
+	logger = loggerRef;
+}
+
 void log::message(const String& message)
 {
 	const String msg = "[Message] " + message;
-	std::cout << msg << std::endl;
+	logger->message(msg);
 }
 
 void log::error(const String& error)
 {
 	const String msg = "[Error] " + error;
-	std::cout << msg << std::endl;
+	logger->error(msg);
 }
