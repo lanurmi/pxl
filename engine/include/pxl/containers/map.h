22b702ff98a3fabec3b956bfc7c09ae7ed4a5196
Joonas Kuusela <kuujoo@gmail.com>
2021-11-07 13:19:24 +0200

    add unordered_set typedef

--- engine/include/pxl/containers/map.h
+++ engine/include/pxl/containers/map.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <unordered_map>
+#include <unordered_set>
 
 namespace pxl
 {
 	template<class T, class E>
 	using Map = std::unordered_map<T, E>;
+
+	template<class T>
+	using Set = std::unordered_set<T>;
 }
