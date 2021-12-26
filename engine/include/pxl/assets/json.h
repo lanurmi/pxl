59d6bce5e5909d2b9b746b8c3455cca9c7db1f81
Joonas Kuusela <kuujoo@gmail.com>
2021-12-26 11:02:15 +0200

    json macros

--- engine/include/pxl/assets/json.h
+++ engine/include/pxl/assets/json.h
@@ -23,8 +23,11 @@ using PxlCustomJson = nlohmann::basic_json<
 namespace pxl
 {
     using Json = PxlCustomJson<>;
-
-    #define PXL_JSON_SERIALIZE(Type, ...)  \
-    friend void to_json(Json& nlohmann_json_j, const Type& nlohmann_json_t) { NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(NLOHMANN_JSON_TO, __VA_ARGS__)) } \
-    friend void from_json(const Json& nlohmann_json_j, Type& nlohmann_json_t) { NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(NLOHMANN_JSON_FROM, __VA_ARGS__)) }
 }
+
+#define PXL_JSON_TO(v1) nlohmann_json_j[#v1] = nlohmann_json_t.v1;
+#define PXL_JSON_FROM(v1) if(nlohmann_json_j.contains(#v1)) nlohmann_json_j.at(#v1).get_to(nlohmann_json_t.v1);
+
+#define PXL_JSON_SERIALIZE(Type, ...)  \
+    friend void to_json(pxl::Json& nlohmann_json_j, const Type& nlohmann_json_t) { NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(NLOHMANN_JSON_TO, __VA_ARGS__)) } \
+    friend void from_json(const pxl::Json& nlohmann_json_j, Type& nlohmann_json_t) { NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(PXL_JSON_FROM, __VA_ARGS__)) }
